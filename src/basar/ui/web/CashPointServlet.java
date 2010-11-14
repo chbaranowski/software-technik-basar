faf9d3e17a2259ff24dd0ff48b49f01b1d8b6b33
tux2323 <developer@noname>
2010-11-14 16:26:00 +0100

    Add Jetty configuration to ant build.xml.

--- src/basar/ui/web/CashPointServlet.java
+++ src/basar/ui/web/CashPointServlet.java
@@ -9,18 +9,21 @@ import javax.servlet.http.HttpServletResponse;
 
 import basar.domain.BasarFacade;
 import basar.domain.BasarFactory;
-import basar.domain.Sale;
 
 public class CashPointServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	BasarFacade basar = BasarFactory.instance.createBasarFacade();
-	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Sale sale = new Sale();
-		basar.purchase(sale);
+		// TODO
 	}
 	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO
+	}
+
 }
