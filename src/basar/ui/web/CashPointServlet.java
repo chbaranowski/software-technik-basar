e34b1d161c73215a8aeb011340a0d3339d7a0b3a
tux2323 <developer@noname>
2010-11-18 22:50:15 +0100

    Add Servlet Logic and Form POST to HTML

--- src/basar/ui/web/CashPointServlet.java
+++ src/basar/ui/web/CashPointServlet.java
@@ -18,12 +18,12 @@ public class CashPointServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO
+		System.out.println("Do Post");
 	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO
+		System.out.println("Do Get");
 	}
 
 }
